a2cdfb3ce5eaed07df2b78065aab1d719c59c73a
Giulio <pilotto.giulio@gmail.com>
2014-03-30 23:47:18 +0200

    Delete

--- Android_app/src/appinventor/ai_andreabrunovial/ZipIt/Screen1.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"YaVersion":"75","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"10","Uuid":"0","AlignHorizontal":"3","BackgroundColor":"&HFF000000","BackgroundImage":"a.jpg","Title":"Screen1","$Components":[{"$Name":"ListPicker1","$Type":"ListPicker","$Version":"6","Uuid":"-772634057","Text":"Choose Device"},{"$Name":"Player1","$Type":"Player","$Version":"4","Uuid":"2021349325","Source":"Allarmenucleare.mp3","Volume":"100"},{"$Name":"BluetoothClient1","$Type":"BluetoothClient","$Version":"5","Uuid":"92661036"}]}}
-|#
